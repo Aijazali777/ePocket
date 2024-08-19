d9e9b26fadc8a856725010fe16bb7608a6494ba3
AliAijaz <aaijaz@trafix.com>
2024-08-19 18:01:15 +0500

    Files added

---
+++ ePocket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ePocket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
